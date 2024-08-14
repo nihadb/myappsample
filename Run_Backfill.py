70044fd92770373e1553e16d65823cc3ee1e4e10
nihadb <40930767+nihadb@users.noreply.github.com>
2024-08-13 20:49:50 -0700

    Create Run_Backfill.py

---
+++ Run_Backfill.py
@@ -0,0 +1,19 @@
+import TiBackfill
+import sys
+
+def main():
+    # Check if the correct number of arguments are provided
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <rundate>")
+        sys.exit(1)
+
+    # Extract the command line arguments
+    runDate = sys.argv[1]
+    isBackfill = sys.argv[2]
+    
+    # Call the function with the parameters
+    TiBackfill.symbol_backfill(runDate, isBackfill)
+    
+
+if __name__ == "__main__":
+    main()
